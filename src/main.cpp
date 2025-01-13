2ace02eba285c6daa863b0f60b5d8bfd8696da5a
Alex-Pennington <alex.pennington@organicengineer.com>
2025-01-13 16:25:19 -0500

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -1016,3 +1016,82 @@ void sendInfo(String payload) {
   send(msgLCDINFO.set(payload.c_str()), configValues.toACK);
   wait(SENDDELAY);
 }
+
+void DS18B20() {
+  //Serial.println("DS18B20()");
+  byte addr[8];
+  int count = 0;
+   
+  while (ds.search(addr)) {
+    byte i;
+    byte type_s;
+    byte data[9];
+    
+    // Serial.print("ROM =");
+    // for( i = 0; i < 8; i++) {
+    //   Serial.write(' ');
+    //   Serial.print(addr[i], HEX);
+    // }
+    
+    switch (addr[0]) {
+      case 0x10:
+        // Serial.println("  Chip = DS18S20");  // or old DS1820
+        type_s = 1;
+        break;
+      case 0x28:
+        // Serial.println("  Chip = DS18B20");
+        type_s = 0;
+        break;
+      case 0x22:
+        // Serial.println("  Chip = DS1822");
+        type_s = 0;
+        break;
+      default:
+        // Serial.println("Device is not a DS18x20 family device.");
+        return;
+    } 
+
+    ds.reset();
+    ds.select(addr);
+    ds.write(0x44, 0);
+    delay(10); 
+    ds.reset();
+    ds.select(addr);    
+    ds.write(0xBE);
+    for ( i = 0; i < 9; i++) { data[i] = ds.read(); }
+    // Convert the data to actual temperature
+    // because the result is a 16 bit signed integer, it should
+    // be stored to an "int16_t" type, which is always 16 bits
+    // even when compiled on a 32 bit processor.
+    int16_t raw = (data[1] << 8) | data[0];
+    if (type_s) {
+      raw = raw << 3; // 9 bit resolution default
+      if (data[7] == 0x10) {
+        // "count remain" gives full 12 bit resolution
+        raw = (raw & 0xFFF0) + 12 - data[6];
+      }
+    } else {
+      byte cfg = (data[4] & 0x60);
+      // at lower res, the low bits are undefined, so let's zero them
+      if (cfg == 0x00) raw = raw & ~7;  // 9 bit resolution, 93.75 ms
+      else if (cfg == 0x20) raw = raw & ~3; // 10 bit res, 187.5 ms
+      else if (cfg == 0x40) raw = raw & ~1; // 11 bit res, 375 ms
+      //// default is 12 bit resolution, 750 ms conversion time
+    }
+    ds18b20Values[count].C = (float)raw / 16.0;
+    ds18b20Values[count].F = ds18b20Values[count].C * 1.8 + 32.0;
+    // Serial.print("Count = ");
+    // Serial.print(count);
+    // Serial.print("  Temperature = ");
+    // Serial.print(ds18b20Values[count].C);
+    // Serial.print(" Celsius, ");
+    // Serial.print(ds18b20Values[count].F);
+    // Serial.println(" Fahrenheit");
+    count = count + 1;
+}
+  // Serial.println("No more addresses.");
+  // Serial.println();
+  ds.reset_search();
+  delay(1000);
+  return;
+}
